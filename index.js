57b57317f1ec8b4df708a51bcf7f93082ba997c8
Aditya Rathore <147582332+AdityaRathore70677@users.noreply.github.com>
2025-11-10 23:49:38 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Resume_Header extends React.Component {
+  render() {
+    return (
+      <h1>Resume Details</h1>
+    );
+  }
+}
+
+export default Resume_Header;
